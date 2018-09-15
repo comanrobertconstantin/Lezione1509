18c45c63fccb3aa7220039b2939ea69703da0b9c
cristianstoian <stoianc801@gmail.com>
2018-09-15 11:46:03 +0200

    Esercizio su tast

---
+++ AppLezione1509/AppLezione1509/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLezione1509
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 55555;
+            Console.WriteLine($"il valore di a è {a}");
+            short s = (short)a;
+            Console.WriteLine($"il valore di s è {s}");
+            Console.ReadLine();
+        }
+    }
+}
